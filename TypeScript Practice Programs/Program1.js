6627ba3e6e459cfeb31fdae135e0cc49ee6811c4
suraj13mj <suraj.janmane131998@gmail.com>
2020-07-12 18:39:13 +0530

    initial commit

---
+++ TypeScript Practice Programs/Program1.js
@@ -0,0 +1,12 @@
+function log(message) {
+    console.log(message);
+}
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Finally :" + i);
+}
+var message = "Hello World..Welcome to TypeScript...!!!";
+log(message);
+doSomething();
---
+++ TypeScript Practice Programs/Program1.ts
@@ -0,0 +1,21 @@
+function log(message)
+{
+    console.log(message);
+}
+
+
+function doSomething(){
+    for (let i = 0; i < 5; i++)
+    {
+        console.log(i);
+    }
+
+    console.log("Finally :"+i);
+}
+
+
+
+var message = "Hello World..Welcome to TypeScript...!!!";
+log(message);
+
+doSomething();
